83b460cb41012a6a8532418372d93c19dfc406bd
YIFAN HOU <u8113538@anu.edu.au>
2026-08-04 15:24:37 +0800

    feat: add RAG retrieval and response workflow

---
+++ rag/vector_store.py
@@ -0,0 +1,107 @@
+import os
+
+from langchain_core.documents import Document
+
+from utils.file_handler import txt_loader, pdf_loader, listdir_with_allowed_type, get_file_md5_hex
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from utils.logger_handler import logger
+from utils.path_tool import get_abs_path
+from utils.config_handler import chroma_config
+from model.factory import embed_model
+
+class VectorStoreService:
+    def __init__(self):
+        self.vector_store = Chroma(
+            collection_name=chroma_config['collection_name'],
+            embedding_function=embed_model,
+            persist_directory=chroma_config['persist_directory'],
+        )
+        self.spliter = RecursiveCharacterTextSplitter(
+            chunk_size=chroma_config['chunk_size'],
+            chunk_overlap=chroma_config['chunk_overlap'],
+            separators=chroma_config['separators'],
+            length_function=len
+        )
+
+    def get_retriever(self):
+        return self.vector_store.as_retriever(search_kwargs = {"k":chroma_config['k']})
+
+    def load_document(self):
+        """
+        从数据文件夹内读取数据文件，转为向量存入向量数据库
+        要计算md5做去重
+        :return: None
+        """
+
+        def check_md5_hex(md5_for_check:str):
+            if not os.path.exists(get_abs_path(chroma_config["md5_hex_store"])):
+                open(get_abs_path(chroma_config["md5_hex_store"]), "w", encoding="utf-8").close()
+                return False
+
+            with open(get_abs_path(chroma_config["md5_hex_store"]), "r", encoding="utf-8") as f:
+                for line in f.readlines():
+                    line = line.strip()
+                    if line == md5_for_check:
+                        return True
+                return False
+
+        def save_md5_hex(md5_for_check:str):
+            with open(get_abs_path(chroma_config["md5_hex_store"]), "a", encoding="utf-8") as f:
+                f.write(md5_for_check + "\n")
+
+        def get_file_documents(read_path:str):
+            if read_path.endswith("txt"):
+                return txt_loader(read_path)
+
+            elif read_path.endswith("pdf"):
+                return pdf_loader(read_path)
+
+            else:
+                return []
+
+        allowed_file_path = listdir_with_allowed_type(
+            get_abs_path(chroma_config["data_path"]),
+            tuple(chroma_config["allow_knowledge_file_type"]),
+        )
+
+        for path in allowed_file_path:
+            md5_hex = get_file_md5_hex(path)
+            if check_md5_hex(md5_hex):
+                logger.info(f"[加载知识库]{path}内容已存在，跳过")
+                continue
+
+            try:
+                documents:list[Document] = get_file_documents(path)
+
+                if not documents:
+                    logger.warning(f"[加载知识库]{path}内没有有效文本内容，跳过")
+                    continue
+
+                split_document:list[Document] = self.spliter.split_documents(documents)
+
+                if not split_document:
+                    logger.warning(f"[加载知识库]{path}分片后没有有效文本内容，跳过")
+                    continue
+
+                #将内容存入向量数据库
+                self.vector_store.add_documents(split_document)
+
+                #记录加载好的文件md5，防止下次重复加载
+                save_md5_hex(md5_hex)
+
+                logger.info(f"[加载知识库]{path}内容加载成功")
+            except Exception as e:
+                #exc_info为True会记录更详细的报错堆栈，如果为False则仅记录报错信息本身
+                logger.error(f"[]{path}加载失败：{str(e)}", exc_info=True)
+                continue
+
+if __name__ == '__main__':
+    vs = VectorStoreService()
+    vs.load_document()
+    retriever = vs.get_retriever()
+    res = retriever.invoke("迷路")
+    for doc in res:
+        print(doc.page_content)
+        print("-"*20)
